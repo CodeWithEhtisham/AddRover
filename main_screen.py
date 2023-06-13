d2a7550a76323b56cf1336e6fcd89715f1ed27f3
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-06-13 11:06:51 +0500

    setup ui with python files

---
+++ main_screen.py
@@ -0,0 +1,35 @@
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication
+import sys
+from os import path
+from PyQt5.uic import loadUiType
+
+FORM_MAIN, _ = loadUiType('ui/main.ui')
+
+
+class MainScreen(QMainWindow, FORM_MAIN):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.Handle_Buttons()
+
+    def Handle_Buttons(self):
+        pass
+
+   
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainScreen()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
