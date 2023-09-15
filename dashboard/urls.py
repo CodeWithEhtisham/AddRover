97c6ab2de2a5a34f46d5f9f6ff8c75e0917157d4
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-09-15 17:51:56 +0500

    ads template added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path("",index,name='index')
+    path("index",index,name='index'),
+    path("", ads, name='ads'),
 ]
